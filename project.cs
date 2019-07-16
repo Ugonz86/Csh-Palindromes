c123b810892b6f4f8a5e333809c8ff472f1d884a
Ugonz86 <ugonzalez86@gmail.com>
2019-07-16 12:14:07 -0700

    This will commit the recent additions

--- project.cs
+++ project.cs
@@ -1,9 +1,51 @@
 using System;
 
-class ProjectName
-{
-  static void Main()
+  class Palabra
   {
-    
+    public string StringPalabra;
+    public Palabra(string palabra)
+    {
+      StringPalabra = palabra;
+    }
+
+    public bool truePal()
+    {
+      char [] arrayPalabra = StringPalabra.ToCharArray();
+      char [] reverseCopy = (char[]) arrayPalabra.Clone();
+
+      Array.Reverse(reverseCopy);
+
+      string stringPalabra = string.Join(",", arrayPalabra);
+      string stringReverse = string.Join(",", reverseCopy);
+
+      if (stringPalabra == stringReverse)
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+    }
   }
-}
+  class Program
+  {
+    static void Main()
+    {
+      Console.WriteLine("Palindrome Tracker");
+      Console.WriteLine("Enter a word: ");
+      string userPalabra = Console.ReadLine();
+
+      Palabra newPalabra = new Palabra (userPalabra);
+
+      if (newPalabra.truePal())
+      {
+        Console.WriteLine("This is a palindrome!");
+      }
+      else
+      {
+        Console.WriteLine("That is not a palindrome!");
+      }
+      Main ();
+      }
+   }
